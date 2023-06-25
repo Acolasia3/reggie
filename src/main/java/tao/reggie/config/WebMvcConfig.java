aeb0531773430221d6809b72523108ba70ddbcaf
Acolasia3 <1934154592@qq.com>
2023-06-26 03:14:37 +0800

    配置swagger

--- src/main/java/tao/reggie/config/WebMvcConfig.java
+++ src/main/java/tao/reggie/config/WebMvcConfig.java
@@ -1,11 +1,18 @@
 package tao.reggie.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import tao.reggie.common.JacksonObjectMapper;
 
 import java.util.List;
@@ -17,21 +24,19 @@ import java.util.List;
 @Slf4j
 @Controller
 public class WebMvcConfig extends WebMvcConfigurationSupport {
-    /**
-     * 设置静态资源映射
-     *
-     * @param registry
-     */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始进行静态资源映射...");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
-    }
 
+    }
     /**
-     * 扩展mvc框架的消息转换器
-     * @param converters
+     * 设置静态资源映射
+     *
+     * @param 
      */
     @Override
     protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -42,5 +47,30 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         messageConverter.setObjectMapper(new JacksonObjectMapper());
         //将上面的消息转换器对象追加到mvc框架的转换器集合中
         converters.add(0,messageConverter);
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        //文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //这里指定Controller扫描包路径
+//                .apis(RequestHandlerSelectors.basePackage("com.github.xiaoymin.knife4j.controller"))
+                //这里指定扫描有ApiOperation注解的类
+                .apis(RequestHandlerSelectors.basePackage("tao.reggie.controller"))
+                //所有路径
+                .paths(PathSelectors.any())
+                //不包含^/inner/.*的路径
+                //.paths(input -> !input.matches("^/inner/.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
     }
 }
